db1b1fb16eb52081d9756353005281f93e28ba12
FarNys <faridsalmaniy@gmail.com>
2021-12-15 08:31:25 -0700

    Fix Redux Wrong Import Reducer

--- src/app/store.js
+++ src/app/store.js
@@ -2,7 +2,7 @@ import { configureStore } from "@reduxjs/toolkit";
 import cardReducer from "../features/cardSlice";
 import userReducer from "../features/userSlice";
 import reviewReducer from "../features/reviewsSlice";
-import dynamicReducer from "../features/dynamicData";
+import dynamicReducer from "../features/dynamicSlice";
 
 export const store = configureStore({
   reducer: {
